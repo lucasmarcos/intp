eebca906fd53a1ed059523895d361f6d746cef72
Lucas da Silva Marcos <640383+lucasmarcos@users.noreply.github.com>
2024-12-07 14:45:48 -0300

    estrutura

--- intp.go
+++ intp.go
@@ -1,5 +1,39 @@
 package intp
 
+func lex(exp string) ([]string, error) {
+	return nil, nil
+}
+
+func parse(tokens []string) (string, error) {
+	return "", nil
+}
+
+func run(exp string) (int, error) {
+	return 0, nil
+}
+
 func Executar(expressao string) (int, bool) {
-	return 0, false
+	if len(expressao) == 0 {
+		return 0, true
+	}
+
+	tokens, err := lex(expressao)
+
+	if err != nil {
+		return 0, true
+	}
+
+	ast, err := parse(tokens)
+
+	if err != nil {
+		return 0, true
+	}
+
+	result, err := run(ast)
+
+	if err != nil {
+		return 0, true
+	}
+
+	return result, false
 }
