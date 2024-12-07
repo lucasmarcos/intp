74874e314fb244f1eeb29b3f82f5121564e10dc3
Lucas da Silva Marcos <640383+lucasmarcos@users.noreply.github.com>
2024-12-07 14:20:15 -0300

    muitos testes

--- intp_test.go
+++ intp_test.go
@@ -3,12 +3,63 @@ package intp_test
 import "testing"
 import "intp"
 
-func TestesBasicos(t *testing.T) {
-	input := ""
+func TestBasicoSoma(t *testing.T) {
+	input := "1+2"
+	expected := 3
 
-	output := intp.Executar(input)
+	output, err := intp.Executar(input)
 
-	if output != false {
-		t.Errorf("%q %v %v", input, output, false)
+	if err != false || output != expected {
+		t.Errorf("%s, %d, %d", input, expected, output)
 	}
+}
+
+func TestBasicoSub(t *testing.T) {
+	input := "5-3"
+	expected := 2
+
+	output, err := intp.Executar(input)
+
+	if err != false || output != expected {
+		t.Errorf("%s, %d, %d", input, expected, output)
+	}
+}
+
+func TestBasicoMult(t *testing.T) {
+}
+
+func TestBasicoDiv(t *testing.T) {
+}
+
+func TestNegativo(t *testing.T) {
+}
+
+func TestPrecedenciaSem(t *testing.T) {
+}
+
+func TestPrecedenciaCom(t *testing.T) {
+}
+
+func TestPrecedenciaAninhado(t *testing.T) {
+}
+
+func TestSintaxeEspaco(t *testing.T) {
+}
+
+func TestSintaxeIncompleto(t *testing.T) {
+}
+
+func TestSintaxeOperadores(t *testing.T) {
+}
+
+func TestSintaxeDesbalanco(t *testing.T) {
+}
+
+func TestSintaxeMisto(t *testing.T) {
+}
+
+func TestSemanticaDivZero(t *testing.T) {
+}
+
+func TestSemanticaVazio(t *testing.T) {
 }
